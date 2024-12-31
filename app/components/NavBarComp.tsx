b65269876fe40f8fbf388a1127f33cd9ea44c821
Vinay Sattineni <vxer1x@gmail.com>
2024-12-31 12:03:24 +0530

    add icon

--- app/components/NavBarComp.tsx
+++ app/components/NavBarComp.tsx
@@ -5,7 +5,10 @@ import './global.css'
 async function NavBarComponent() {
     return (
         <div className="NavBar">
-            <Link href={'/'}><h1 className="Logo">CineLogs.</h1></Link>
+            <Link href={'/'}>
+            <h1 className="Logotitle">CineLogs.</h1>
+            <img src="/popcorn.png" alt="popcorn"/>
+            </Link>
             
             <input type="text" className="SearchBar" placeholder="Search..."/>
         </div>
