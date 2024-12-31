5730133687ca738621693b0c458ce7c8e994c1c6
Vinay Sattineni <vxer1x@gmail.com>
2024-12-31 17:23:06 +0530

    ADS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css';
+import Script from 'next/script';
 
 // app/layout.tsx
 export const metadata = {
@@ -14,17 +15,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Link to favicon */}
+        {/* Metadata */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Indian Movies Info." />
+        <meta name="author" content="vxer.info" />
+        
+        {/* Favicon */}
         <link rel="icon" href="/popcorn.png" type="image/png" />
         
-        {/* Google AdSense script */}
-        <script
-          async
+        {/* Google AdSense */}
+        <Script
+          id="adsense-script"
+          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4613964821229195"
           crossOrigin="anonymous"
-        ></script>
+        />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
