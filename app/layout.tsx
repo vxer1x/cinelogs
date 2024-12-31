36524f4fa1c7347700a525da5fee70aa3689b4ae
Vinay Sattineni <vxer1x@gmail.com>
2024-12-31 17:37:21 +0530

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,56 +19,16 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Indian Movies Info." />
         <meta name="author" content="vxer.info" />
-
+        
         {/* Favicon */}
         <link rel="icon" href="/popcorn.png" type="image/png" />
-
+        
         {/* Google AdSense */}
         <Script
           id="adsense-script"
           strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4613964821229195"
           crossOrigin="anonymous"
-        />
-
-        {/* Monetag Script */}
-        <Script
-          id="monetag-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                var m = document.createElement('script');
-                m.type = 'text/javascript';
-                m.async = true;
-                m.src = 'https://alwingulla.com/88/tag.min.js';
-                m.setAttribute('data-zone', '122639');
-                m.setAttribute('data-cfasync', 'false');
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(m, s);
-              })();
-            `,
-          }}
-        />
-
-        {/* Service Worker Registration */}
-        <Script
-          id="service-worker-registration"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('serviceWorker' in navigator) {
-                navigator.serviceWorker
-                  .register('/sw.js')
-                  .then(function (registration) {
-                    console.log('Service Worker registered with scope:', registration.scope);
-                  })
-                  .catch(function (error) {
-                    console.error('Service Worker registration failed:', error);
-                  });
-              }
-            `,
-          }}
         />
       </head>
       <body>
