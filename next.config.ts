4485053925ee21aef4bfa65760e00f712cd01b4c
Vinay Sattineni <vxer1x@gmail.com>
2025-01-03 16:32:57 +0530

    real

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Ignores all ESLint errors during builds
   },
-  // Other Next.js configurations
+  /* other config options here */
 };
 
-// Export the Next.js config along with sitemap configuration
-module.exports = {
-  ...nextConfig,
-  siteUrl: 'https://vxer.info', // Your site URL
-  generateRobotsTxt: true, // Optionally generate robots.txt
-  // You can add other options like changefreq, priority, etc.
-};
+export default nextConfig;
